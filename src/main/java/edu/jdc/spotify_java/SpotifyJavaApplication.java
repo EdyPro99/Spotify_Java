cfd993ba128818b37f7d2f9b14f41d51f848676c
Edy Prospero <edypro5465@gmail.com>
2026-03-10 14:06:56 -0500

    Creacion Proyecto Git

---
+++ src/main/java/edu/jdc/spotify_java/SpotifyJavaApplication.java
@@ -0,0 +1,13 @@
+package edu.jdc.spotify_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotifyJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/jdc/spotify_java/SpotifyJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.jdc.spotify_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
